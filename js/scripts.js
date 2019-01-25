f439b14784d2503bd2e5a92c3dff12fd7389a4a1
Nayomi Morita <nayomi.morita@gmail.com>
2019-01-25 11:02:10 -0800

    add frontend javascript

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,36 @@ function numCounter(input){
 
   }
   numberOutput +=
-  translator(numArray[0])
-  return numberOutput;
-  console.log(numArray)
+  translator(numArray[0],"0, ","")
+  numberOutput +=
+  translator(numArray[1],"1, ","hi")
+  numberOutput +=
+  translator(numArray[2],"2, ","hi")
+  numberOutput +=
+  translator(numArray[3],"3","hi")
+  numberOutput +=
+  console.log(numberOutput)
 }
 
 console.log(numCounter("9"))
 
-function translator(num){
-  if (num===3){
-    return "hi"
+function translator(num,nar,msg){
+  if (num===0){
+    return nar;
+  } else if (num===1){
+    return nar;
+  } else if (num===2){
+    return nar;
+  } else if (num===3){
+    return msg;
   }
 }
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#bbInput").val();
+    var numOutput = numCounter(userInput);
+    $("p#bbOytput").text(numOutput);
+  });
+});
