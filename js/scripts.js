20fe11340e3161f9d203370acf8a9cac63ff8f5d
Nayomi Morita <nayomi.morita@gmail.com>
2019-01-25 11:16:36 -0800

    add fixes to js, still not functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,44 @@
+function translator(num,char,spChar,xSpChar){
+  if (num==0){
+    return "";
+  } else if(num==1){
+    return char;
+  } else if(num==2){
+    return char+char;
+  } else if(num==3){
+    return char+char+char;
+  } else if(num==4){
+    return char+spChar;
+  } else if(num==5){
+    return spChar;
+  } else if (num==6){
+    return spChar+char;
+  } else if (num==7){
+    return spChar+char+char;
+  } else if (num==8){
+    return spChar+char+char+char;
+  } else if (num==9){
+    return char+xSpChar;
+  }
+}
+
+function romanizer(number){
+  if (parseInt(number)<4000){
+    var romanNumeralOutput="";
+    var numArray=number.split("");
+    for (var i=0;numArray.length<4;i++){
+      numArray.unshift("0")
+    }
+    romanNumeralOutput += translator(numArray[0],"M","Y","Z");
+    romanNumeralOutput += translator(numArray[1],"C","D","M");
+    romanNumeralOutput += translator(numArray[2],"X","L","C");
+    romanNumeralOutput += translator(numArray[3],"I","V","X");
+    return romanNumeralOutput;
+  } else {
+    alert("Alert! Roman Numerals don't go past 3999!");
+  }
+}
+
 function numCounter(input){
   var numberOutput
   var number = parseInt(input);
@@ -36,7 +77,13 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var userInput = $("input#bbInput").val();
-    var numOutput = numCounter(userInput);
-    $("p#bbOytput").text(numOutput);
+    var userOutput = numCounter(userInput);
+    $("p#bbOutput").text(userOutput);
+  });
+  $("form#romeForm").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#romeInput").val();
+    var outputSentence = romanizer(userInput);
+    $("p#romeOutput").text(outputSentence).show();
   });
 });
