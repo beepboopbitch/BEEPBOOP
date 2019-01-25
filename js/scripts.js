28814c34a56c0421309db030e2683c21844452ea
Nayomi Morita <nayomi.morita@gmail.com>
2019-01-25 10:14:07 -0800

    add function translator

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 function numCounter(input){
+  var numberOutput
   var number = parseInt(input);
   var numArray = []
   for (var i=0;i<=number;i++){
     numArray.push(i)
-    console.log(numArray)
+
+  }
+  numberOutput +=
+  translator(numArray[0])
+  return numberOutput;
+  console.log(numArray)
+}
+
+console.log(numCounter("9"))
+
+function translator(num){
+  if (num===3){
+    return "hi"
   }
 }
-console.log(numCounter("9"))
